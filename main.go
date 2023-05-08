b6632dd5361c94b7b9f93ab116ff692e1ad8d113
yazdani <hosseinyazdani1376@gmail.com>
2023-05-08 11:07:01 +0330

    run simple app

--- main.go
+++ main.go
@@ -1,20 +1,9 @@
 package main
 
 import (
-	"log"
-	"net/http"
+	"fmt"
 )
 
-// this is a simple web server
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World"))
-	})
-
-	log.Println("Starting server on port 8000")
-	err := http.ListenAndServe(":8000", mux)
-	if err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println("Hello, GitHub Actions!")
 }
